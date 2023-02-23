43b77fed354c5148fb0452662ec2caab47f81467
geoport <destek@geoport.com.tr>
2023-02-23 16:23:14 +0300

    docstring added

--- src/helper.py
+++ src/helper.py
@@ -8,6 +8,21 @@ latitudes = df["Enlem"].to_frame()
 
 # PGV values doesn't match with the values with the AFAD website.
 def interpolate(head, dyhd_no, x_f, y_f, variables):
+    """
+    This function performs bi-linear interpolation on the input variables.
+
+    Args:
+        head (str): A string representing the parameter name, such as "S1", "SS", "PGA", or "PGV".
+        dyhd_no (int): An integer representing the DYHD number, available values are 1,2,3,4.
+        x_f (float): A float representing the fractional distance between x1 and x2.
+        y_f (float): A float representing the fractional distance between y1 and y2.
+        variables (list): A list of Pandas dataframes representing the four variables that
+                          need to be interpolated.
+
+    Returns:
+        float: The interpolated value of the parameter.
+
+    """
     var1 = variables[0][f"{head}{dyhd_no}"].iloc[0]
     var2 = variables[1][f"{head}{dyhd_no}"].iloc[0]
     var3 = variables[2][f"{head}{dyhd_no}"].iloc[0]
@@ -20,6 +35,19 @@ def interpolate(head, dyhd_no, x_f, y_f, variables):
 
 
 def get_spectral_values(dyhd_no, latitude, longitude):
+    """
+    This function calculates the spectral values for a given DYHD number, latitude, and longitude
+    using bi-linear interpolation.
+
+    Args:
+        dyhd_no (int): An integer representing the DYHD number, available values are 1,2,3,4.
+        latitude (float): A float representing the latitude value.
+        longitude (float): A float representing the longitude value.
+
+    Returns:
+        Tuple: A tuple containing four floats representing the SS, S1, PGA, and PGV values.
+
+    """
     y, x = latitude, longitude
 
     df_sort_y = latitudes.iloc[(latitudes["Enlem"] - y).abs().argsort()[:2]]
--- src/main.py
+++ src/main.py
@@ -14,6 +14,8 @@ app.add_middleware(
 
 @app.get("/seismic_hazard")
 async def api_handler(latitude: float, longitude: float, dyhd_no: int):
+    if dyhd_no not in [1, 2, 3, 4]:
+        return {"error": "DYHD number must be 1, 2, 3, or 4."}
     ss, s1, pga, pgv = get_spectral_values(dyhd_no, latitude, longitude)
 
     return {"PGA": pga, "PGV": pgv, "SS": ss, "S1": s1}
